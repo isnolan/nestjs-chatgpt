ff7e0c8c2772a6f023e62bd4404458bf14fea1f3
yhostc <yhostc@gmail.com>
2023-02-22 15:24:47 +0800

    feat: change default host

--- src/common/adapters/redis-io.adapter.ts
+++ src/common/adapters/redis-io.adapter.ts
@@ -7,7 +7,7 @@ export class RedisIoAdapter extends IoAdapter {
   private adapterConstructor: ReturnType<typeof createAdapter>;
 
   async connectToRedis(): Promise<void> {
-    const pubClient = createClient({ url: `redis://127.0.0.1:6379` });
+    const pubClient = createClient({ url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}` });
     const subClient = pubClient.duplicate();
 
     await Promise.all([pubClient.connect(), subClient.connect()]);
