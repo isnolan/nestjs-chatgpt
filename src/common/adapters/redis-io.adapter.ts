6bc2e0847b6068ff736b52b1df90bfb8c9678e30
yhostc <yhostc@gmail.com>
2023-02-28 22:50:42 +0800

    feat: unified config for redis

--- src/common/adapters/redis-io.adapter.ts
+++ src/common/adapters/redis-io.adapter.ts
@@ -2,14 +2,15 @@ import { IoAdapter } from '@nestjs/platform-socket.io';
 import { ServerOptions } from 'socket.io';
 import { createAdapter } from '@socket.io/redis-adapter';
 import { createClient } from 'redis';
-
+import config from '../../config/configuration';
 export class RedisIoAdapter extends IoAdapter {
   private adapterConstructor: ReturnType<typeof createAdapter>;
 
   async connectToRedis(): Promise<void> {
-    const REDIS_HOST = process.env.REDIS_HOST;
-    const REDIS_PORT = parseInt(process.env.REDIS_PORT || '6379');
-    const pubClient = createClient({ url: `redis://${REDIS_HOST}:${REDIS_PORT}` });
+    const { host, port } = config().redis;
+    const url = `redis://${host}:${port}`;
+    console.log(`->`, host, port, url);
+    const pubClient = createClient({ url });
     const subClient = pubClient.duplicate();
 
     await Promise.all([pubClient.connect(), subClient.connect()]);
